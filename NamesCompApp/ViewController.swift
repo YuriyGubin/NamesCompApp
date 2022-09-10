d7e09052f937891a8b67253627984fb38a3ab63b
YuriyGubin <iosdevelop.2022@gmail.com>
2022-09-10 15:13:06 +0500

    Initial Commit

---
+++ NamesCompApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesCompApp
+//
+//  Created by Yuriy on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
